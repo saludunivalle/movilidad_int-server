85845d1de39019205e839959f8a026b678b9a47e
Isabela_ <isabela.rosero@correounivalle.edu.co>
2024-07-03 19:55:42 -0500

    Fix credentials and update code

--- index.js
+++ index.js
@@ -10,12 +10,14 @@ const app = express();
 const router = express.Router();
 const PORT = process.env.PORT || 3001;
 
+const privateKey = process.env.private_key.replace(/\\n/g, '\n');
+
 const auth = new google.auth.GoogleAuth({
   credentials: {
     type: process.env.type,
     project_id: process.env.project_id,
     private_key_id: process.env.private_key_id,
-    private_key: process.env.private_key.replace(/\\n/g, '\n'),
+    private_key: privateKey,
     client_email: process.env.client_email,
     client_id: process.env.client_id,
     auth_uri: process.env.auth_uri,
